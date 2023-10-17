681ef35cf14b62ebc4a7f5f784dbf08f0c392702
AnoopBhandal <anoopbhandal1@gmail.com>
2023-10-17 12:12:38 +0100

    added file structure

--- src/app.jsx
+++ src/app.jsx
@@ -9,8 +9,12 @@ export default function Musician(){
 
     const likeSong = () => {
         setLiked(!liked)
-        console.log(liked);
-    }
+        return(
+        <div 
+        style = {{backgroundColor:  liked ? 'red' : 'blue' 
+        }}
+        />
+    )}
 
 
     return (
@@ -21,11 +25,11 @@ export default function Musician(){
             <p>P Money is South East London's grime MC known for his high octane live performances, distinctive, punchline based freestyles. He has a style that suits both Dubstep & Grime and even Drum n Bass.</p>
             <h2>Albums</h2>
             <img className="album" src={"https://f4.bcbits.com/img/a1841862943_16.jpg"}/>
-            <p>Streets, Love & Other Stuff (2023) {liked ? "Liked" : "Unliked"} </p>
+            <p>Streets, Love & Other Stuff (2023) Album {liked ? "Liked" : "Not liked"} </p>
             {/* <button><img onClick={unlikeSong} className = "heart" src={"https://thenounproject.com/api/private/icons/773080/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0"}/></button>   */}
-            <button><img onClick={() => likeSong()} 
+            <button className="likeButton"><img onClick={() => likeSong()} 
             className = "heart" src={"https://thenounproject.com/api/private/icons/773080/edit/?backgroundShape=SQUARE&backgroundShapeColor=%23000000&backgroundShapeOpacity=0&exportSize=752&flipX=false&flipY=false&foregroundColor=%23000000&foregroundOpacity=1&imageFormat=png&rotation=0"}/>
-            {liked ? "Liked" : "Unliked" }
+            {liked ? "Liked" : "Not liked" }
             </button>  
             {/* <button><img onClick={likeSong} className = "heart" src={"https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Heart_coraz%C3%B3n.svg/1024px-Heart_coraz%C3%B3n.svg.png"}/></button>   */}
         </>
